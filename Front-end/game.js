91d7c456c7572ab62501d6f8bfd8b63505d0eda2
“blackyjon” <jwrocks@ymail.com>
2020-09-13 19:59:13 -0600

    Introducing rock paper scissors

---
+++ Front-end/game.js
@@ -0,0 +1,125 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img')
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function () {
+                this.style.animation = '';
+            })
+        })
+
+        //comuter options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function () {
+                //computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                //Adds delay after animation
+                setTimeout(() => {
+                    //here is where we call compare hands
+                    compareHands(this.textContent, computerChoice);
+
+                    //update images
+                    playerHand.src = `./imgs/assets/${this.textContent}.png`;
+                    computerHand.src = `./imgs/assets/${computerChoice}.png`;
+                }, 2000);
+
+                //Animation
+                playerHand.style.animation = 'shakePlayer 2s ease';
+                computerHand.style.animation = 'shakeComputer 2s ease';
+            })
+        });
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+
+        //Resets game after score reaches 10
+        if (pScore === 10 || cScore === 10) {
+            pScore = 0;
+            cScore = 0;
+            return;
+        }
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update text
+        const winner = document.querySelector('.winner');
+        //checking for a tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie';
+            return;
+        }
+        //check for rock
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        //check for paper
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        //check for scissors
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+    }
+    //inner functions
+    startGame();
+    playMatch();
+};
+
+
+game();
